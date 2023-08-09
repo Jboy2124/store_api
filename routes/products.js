ee8a3c3bbb0663cb91c2a3799c627a94b2df7b97
jboy <jufelabecia@gmail.com>
2023-08-09 13:11:39 +0800

    refactor: auth and jwt expiry

--- routes/products.js
+++ routes/products.js
@@ -19,7 +19,7 @@ const {
 
 module.exports = router
   //endpoints
-  .post("/product/new", auth, image(), post)
+  .post("/product/new", image(), post)
   .post("/product/list/includes", auth, postList)
   .get("/product/image/:id", getImage)
   .get("/products/feature/:id", getFeatProd)
--- utils/jwt.js
+++ utils/jwt.js
@@ -8,7 +8,7 @@ module.exports = {
         process.env.JWT_ACCESS_TOKEN_KEY,
         {
           algorithm: "HS384",
-          expiresIn: "20s",
+          expiresIn: "1d",
         }
       );
       return token;
